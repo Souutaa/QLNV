9b173f16b8445ec28e03c8f6e2176302414beeea
Quang Long <trinhq011@gmail.com>
2021-12-09 22:28:34 +0700

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author 84898
+ */
+public abstract class Abstract {
+    
+    
+    public abstract void docFile(String File);
+    
+    public abstract void vietFile(String File)throws IOException;
+    
+    
+}
---
+++ Interface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author 84898
+ */
+public interface Interface {
+    
+    public abstract void hienthi();
+    public abstract void sua();
+    public abstract void nhap();
+}
